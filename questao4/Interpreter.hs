cfbb55efce0dc1f9153c78b7e75bd5d916708f02
Gabriel Cesário <gcesario031@gmail.com>
2021-04-21 17:18:51 -0300

    Implementacao da Questao 4
    A questao 4 foi implementada. Adicionado suporte para o statement
    try/catch no interpretador, assim como suporte para mensagens de
    erro nesta questao.

--- questao4/Interpreter.hs
+++ questao4/Interpreter.hs
@@ -10,7 +10,7 @@ type ErrorMessage = String
    mas a sua definicao (corpo) pode ficar a mesma
    executeP :: RContext -> Program  -> Either ErrorMessage RContext
 -}
-executeP :: RContext -> Program  -> RContext
+executeP :: RContext -> Program  -> Either ErrorMessage RContext
 executeP context (Prog stm) = execute context stm
    
 
@@ -21,27 +21,56 @@ executeP context (Prog stm) = execute context stm
  e, consequentemente o execute tambem. Assim, todos tipos de comandos 
  serao afetados
 -}
-execute :: RContext -> Stm -> RContext
-execute context x = case x of
-   SAss id exp -> update context (getStr id) (eval context exp)
-   SBlock [] -> context
-   SBlock (s:stms) -> execute (execute context s) (SBlock stms) 
-   SWhile exp stm -> if ( (eval context exp) /= 0) 
-                      then execute (execute context stm) (SWhile exp stm)
-                      else context
 
+execute :: RContext -> Stm -> Either ErrorMessage RContext
+execute context x = case x of
+   SAss id exp -> case (eval context exp) of
+                     Left errorMsg -> Left errorMsg
+                     Right numResult -> Right (update context (getStr id) numResult)
+   ------------------------------------------------------------------
+   SBlock [] -> Right context
+   SBlock (s:stms) -> case (execute context s) of
+                        Left msg -> Left msg
+                        Right result -> execute result (SBlock stms)
+   ------------------------------------------------------------------
+   SWhile exp stm -> case (eval context exp) of
+                        Left errorMsg -> Left errorMsg
+                        Right numResult -> if (numResult /= 0)
+                                          then (case (execute context stm) of
+                                                   Left stmErrorMsg -> Left stmErrorMsg
+                                                   Right stmContextResult -> execute stmContextResult (SWhile exp stm))
+                                          else Right context
+   ------------------------------------------------------------------
+   STry (sTry:stmsTry) stmtsCatch stmtsFinal -> case (execute context sTry) of
+                                                         Left _ -> case (execute context (SBlock stmtsCatch)) of
+                                                                     Left errorMsg -> Left errorMsg
+                                                                     Right catchContextResult -> execute catchContextResult (SBlock stmtsFinal)
+                                                         Right tryContextResult -> execute tryContextResult (STry stmsTry stmtsCatch stmtsFinal)
+   STry [] stmtsCatch stmtsFinal -> execute context (SBlock stmtsFinal)
+
+getResult context exp0 exp funcao = case eval context exp0 of
+                        Left msg0 -> Left msg0
+                        Right result0 -> (case eval context exp of
+                                             Left msg1 -> Left msg1
+                                             Right result1 -> Right (funcao result0 result1))
 
 {- Dica: o tipo de eval deve mudar para
  eval :: RContext -> Exp -> Either ErrorMessage Integer
 -}
-eval :: RContext -> Exp -> Integer
+eval :: RContext -> Exp -> Either ErrorMessage Integer
 eval context x = case x of
-    EAdd exp0 exp  -> eval context exp0 + eval context exp
-    ESub exp0 exp  -> eval context exp0 - eval context exp
-    EMul exp0 exp  -> eval context exp0 * eval context exp
-    EDiv exp0 exp  -> eval context exp0 `div` eval context exp
-    EInt n  -> n
-    EVar id  -> lookup context (getStr id)
+    EAdd exp0 exp  -> getResult context exp0 exp (+)
+    ESub exp0 exp  -> getResult context exp0 exp (-)
+    EMul exp0 exp  -> getResult context exp0 exp (*) 
+    EDiv exp0 exp -> (case (eval context exp0) of
+                        Right n -> (case (eval context exp) of 
+                                       Left msg1 -> Left msg1
+                                       Right m -> if (m /= 0)
+                                                   then Right (n `div` m) 
+                                                   else Left ("divisao por 0"))
+                        Left msg -> Left msg)
+    EInt n  -> Right n
+    EVar id  -> Right (lookup context (getStr id))
 {-  algumas dicas abaixo...para voce adaptar o codigo acima
     EDiv e1 e2 -> case eval context e1 of 
                     Right ve1 -> case eval context e2 of 
